2651a1a61e5660bc8c872260aa82dd04bf4ec5ca
npcoderes <nikulpanchal2505@gmail.com>
2025-09-02 11:49:18 +0530

    change_cors

--- server/db/db.js
+++ server/db/db.js
@@ -5,8 +5,8 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {  
-    rejectUnauthorized: false,  
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(cors(
   {
-    origin: "http://localhost:5173",
+    origin: "https://taskmate-frontend-kappa.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true 
   }
